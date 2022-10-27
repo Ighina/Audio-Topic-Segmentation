cc1f1d5cad22197c78f27b000feaf292b949adb2
Iacopo Ghinassi <45108036+Ighina@users.noreply.github.com>
2022-10-27 16:08:39 +0100

    Update run_uniform_extraction_BMAT.sh

--- run_uniform_extraction_BMAT.sh
+++ run_uniform_extraction_BMAT.sh
@@ -8,24 +8,24 @@ for encoder in "${encoders[@]}"
     do
         if [[ ${encoder} == "x-vectors" ]]; then
             expname=${expdir}/x-vectors
-            python extract_embeddings.py -data OpenBMAT/BMAT-ATS_transcripts  -audio OpenBMAT/BMAT-ATS -od ${expname} -lod ${lab_out} -lab data/OpenBMAT/BMAT-ATS_labels.json -vb -sb -ui ${uniform_time} -vd --BMAT --cont
+            python extract_embeddings.py -data OpenBMAT/BMAT-ATS_transcripts  -audio OpenBMAT/BMAT-ATS -od ${expname} -lod ${lab_out} -lab OpenBMAT/BMAT-ATS_labels.json -vb -sb -ui ${uniform_time} -vd --BMAT --cont
         elif [[ ${encoder} == "openl3" ]]; then
             expname=${expdir}/openl3
-            python extract_embeddings.py -data OpenBMAT/BMAT-ATS_transcripts -audio OpenBMAT/BMAT-ATS -od ${expname} -lod ${lab_out} -lab data/OpenBMAT/BMAT-ATS_labels.json -vb -sb -ui ${uniform_time} -vd --openl3 --BMAT --cont
+            python extract_embeddings.py -data OpenBMAT/BMAT-ATS_transcripts -audio OpenBMAT/BMAT-ATS -od ${expname} -lod ${lab_out} -lab OpenBMAT/BMAT-ATS_labels.json -vb -sb -ui ${uniform_time} -vd --openl3 --BMAT --cont
         elif [[ ${encoder} == "prosodic" ]]; then
             expname=${expdir}/prosodic
-            python extract_embeddings.py -data OpenBMAT/BMAT-ATS_transcripts -audio OpenBMAT/BMAT-ATS -od ${expname} -lod ${lab_out} -lab data/OpenBMAT/BMAT-ATS_labels.json -vb -sb -ui ${uniform_time} -vd --prosodic --BMAT --cont
+            python extract_embeddings.py -data OpenBMAT/BMAT-ATS_transcripts -audio OpenBMAT/BMAT-ATS -od ${expname} -lod ${lab_out} -lab OpenBMAT/BMAT-ATS_labels.json -vb -sb -ui ${uniform_time} -vd --prosodic --BMAT --cont
         elif [[ ${encoder} == "mfcc" ]]; then
             expname=${expdir}/mfcc
-            python extract_embeddings.py -data OpenBMAT/BMAT-ATS_transcripts -audio OpenBMAT/BMAT-ATS -od ${expname} -lod ${lab_out} -lab data/OpenBMAT/BMAT-ATS_labels.json -vb -sb -ui ${uniform_time} -vd --mfcc --BMAT --cont
+            python extract_embeddings.py -data OpenBMAT/BMAT-ATS_transcripts -audio OpenBMAT/BMAT-ATS -od ${expname} -lod ${lab_out} -lab OpenBMAT/BMAT-ATS_labels.json -vb -sb -ui ${uniform_time} -vd --mfcc --BMAT --cont
         elif [[ ${encoder} == "wav2vec" ]]; then
             expname=${expdir}/wav2vec
-            python extract_embeddings.py -data OpenBMAT/BMAT-ATS_transcripts -audio OpenBMAT/BMAT-ATS -od ${expname} -lod ${lab_out} -lab data/OpenBMAT/BMAT-ATS_labels.json -vb -sb -ui ${uniform_time} -vd --wav2vec --BMAT --cont
+            python extract_embeddings.py -data OpenBMAT/BMAT-ATS_transcripts -audio OpenBMAT/BMAT-ATS -od ${expname} -lod ${lab_out} -lab OpenBMAT/BMAT-ATS_labels.json -vb -sb -ui ${uniform_time} -vd --wav2vec --BMAT --cont
         elif [[ ${encoder} == "crepe" ]]; then
             expname=${expdir}/crepe
-            python extract_embeddings.py -data OpenBMAT/BMAT-ATS_transcripts -audio OpenBMAT/BMAT-ATS -od ${expname} -lod ${lab_out} -lab data/OpenBMAT/BMAT-ATS_labels.json -vb -sb -ui ${uniform_time} -vd --CREPE --BMAT --cont
+            python extract_embeddings.py -data OpenBMAT/BMAT-ATS_transcripts -audio OpenBMAT/BMAT-ATS -od ${expname} -lod ${lab_out} -lab OpenBMAT/BMAT-ATS_labels.json -vb -sb -ui ${uniform_time} -vd --CREPE --BMAT --cont
         else
             expname=${expdir}/ecapa
-            python extract_embeddings.py -data OpenBMAT/BMAT-ATS_transcripts -audio OpenBMAT/BMAT-ATS -od ${expname} -lod ${lab_out} -lab data/OpenBMAT/BMAT-ATS_labels.json -vb -sb -ui ${uniform_time} -vd --ecapa --BMAT --cont
+            python extract_embeddings.py -data OpenBMAT/BMAT-ATS_transcripts -audio OpenBMAT/BMAT-ATS -od ${expname} -lod ${lab_out} -lab OpenBMAT/BMAT-ATS_labels.json -vb -sb -ui ${uniform_time} -vd --ecapa --BMAT --cont
         fi
     done
