48c8b485f9703bb0b9f78a42bec3769fa9bba09b
Iacopo Ghinassi <45108036+Ighina@users.noreply.github.com>
2022-12-07 22:15:28 +0000

    Add files via upload

--- RadioNews-BBC/run_radionews_exp.sh
+++ RadioNews-BBC/run_radionews_exp.sh
@@ -1,12 +1,11 @@
 bs=$1
 expdir=$2
 metric=$3
-seg=$4
 
-declare -a encoders=(x-vectors openL3/_mean_std openL3/_delta_gap openL3/_last openL3/_max prosodic mfcc Wav2Vec/_mean_std Wav2Vec/_delta_gap Wav2Vec/_last Wav2Vec/_max CREPE/_mean_std CREPE/_delta_gap CREPE/_last CREPE/_max)
+declare -a encoders=(x-vectors openl3/_mean_std openl3/_delta_gap openl3/_last openl3/_max prosodic mfcc wav2vec/_mean_std wav2vec/_delta_gap wav2vec/_last wav2vec/_max crepe/_mean_std crepe/_delta_gap crepe/_last crepe/_max)
 
 for encoder in "${encoders[@]}"
     do
         expname=${expdir}/BiLSTM_bs${bs}_${encoder}
-        python ../train_fit.py -exp ${expname} -s_last -arc BiLSTM -enc ${encoder} -lr 1e-3 -hs -huss 128 -nlss 2 -diss 0 -doss 0 -data RadioNews -bs ${bs} -ef PodcastUniform1/${encoder}_${seg} -lf PodcastUniform1/labs_dict.pkl --metric ${metric} -loss BinaryCrossEntropy -max 1000 -vp 0.15 -pat 50 -ar -as -kcv 48 -msk -msk_pr 0.70
+        python ../train_fit.py -exp ${expname} -s_last -arc BiLSTM -enc ${encoder} -lr 1e-3 -hs -huss 256 -nlss 2 -diss 0 0.2 0.5 -doss 0 0.2 0.5 -data RadioNews -bs ${bs} -ef RadioNewsUniform1/${encoder} -lf RadioNewsUniform1/labs_dict.pkl --metric ${metric} -max 1000 -vp 0.15 -pat 50 -ar -as -split RadioNews_split.json -loss FocalLoss
     done
